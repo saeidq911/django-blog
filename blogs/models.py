6df0ec200a18d3a017add47fee977290475c7522
saeidq911 <saeidqozatlu@outlook.com>
2023-06-19 15:44:45 +0330

    blogs and search features

--- blogs/models.py
+++ blogs/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+
 class Category(models.Model):
     category_name = models.CharField(max_length=50 , unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
