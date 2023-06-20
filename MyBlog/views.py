6cdbf864e73ede7f2d40bb7d12e0a9a4f351f349
saeidq911 <saeidqozatlu@outlook.com>
2023-06-20 15:08:28 +0330

    custom dashboard and category functionality

--- MyBlog/views.py
+++ MyBlog/views.py
@@ -38,7 +38,7 @@ def login(request):
 
             if user is not None:
                 auth.login(request,user)
-            return redirect('home')
+            return redirect('dashboard')
     form = AuthenticationForm()
     context = {
         'form': form,
