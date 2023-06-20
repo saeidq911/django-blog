62f6c906580e8877857e101ba45548fa7798e6be
saeidq911 <saeidqozatlu@outlook.com>
2023-06-20 18:06:13 +0330

    blog CRUD features on custom dashboard

--- dashboards/urls.py
+++ dashboards/urls.py
@@ -2,8 +2,15 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.dashboard , name='dashboard'),
+    #category crud
     path('categories/',views.categories,name='categories'),
     path('categories/add/', views.add_category,name='add_category'),
     path('categories/edit/<int:pk>/', views.edit_category,name='edit_category'),
     path('categories/delete/<int:pk>/', views.delete_category,name='delete_category'),
+
+    #posts crud
+    path('posts/', views.posts,name='posts'),
+    path('posts/add/', views.add_posts,name='add_posts'),
+    path('posts/edit/<int:pk>/', views.edit_posts,name='edit_posts'),
+    path('posts/delete/<int:pk>/', views.delete_posts,name='delete_posts'),
 ]
